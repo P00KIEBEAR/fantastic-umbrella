09ad47134ba6faa4504a5478a4192c355158b891
pat paggi <patpaggi@gmail.com>
2021-01-07 18:51:20 -0700

    env

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,6 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    username: 'root',
-    password: 'password',
     host: 'localhost',
     dialect: 'mysql',
     dialectOptions: {
